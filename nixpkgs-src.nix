9d5e676424c0a41e3fee7ded76b2d22ca28ed572
Superfly Johnson <superfly.johnson@outlook.com>
2022-07-16 13:35:43 -0400

    Updated NixPkgs Source to 22.05

--- nixpkgs-src.nix
+++ nixpkgs-src.nix
@@ -1,7 +1,7 @@
 builtins.fetchGit {
   name = "nixpkgs-for-craifty";
-  url = https://github.com/nixos/nixpkgs-channels/;
-  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-19.09`
-  ref = "refs/heads/nixos-19.09";
-  rev = "dca7ec628e55307ac4b46f00f3be09464fcf4f4b";
+  url = https://github.com/nixos/nixpkgs/;
+  # `git ls-remote https://github.com/nixos/nixpkgs release-20.05`
+  ref = "refs/heads/release-22.05";
+  rev = "aeb21e4ec9c81ed34b72fda5bf60e5780992c2c9";
 }
